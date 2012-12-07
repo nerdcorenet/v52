fe846d4f1ef975b892c775c11127db806a349410
Chris Sibbitt <chris@fractalcube.net>
2012-12-07 16:37:12 -0500

    split card into basic model and a view decorator so that it's code can be shared client vs. server in prepatation for the server... serving cards whose value is unknown to the client

---
+++ v52CardView.js
@@ -0,0 +1,45 @@
+//Decorates a card object to be used client-side
+v52.CardView = function(card){
+
+	card.view = new Kinetic.Rect({ 
+                                width: 72,
+                                height: 96,
+                                fill: "green",
+                                stroke: "black",
+                                strokeWidth: 1,
+                                draggable: true,
+				x: 0,
+				y: 0
+                        });
+
+	card.view.v52_parent = card;
+	card.view.image = new Image();
+	card.view.on('dblclick', 	
+		function(evt){ 
+			this.v52_parent.flip();
+			this.v52_parent.refresh();
+		}
+	);
+
+	card.refresh = function(){
+		if(this.face == 'up'){
+			this.view.image.src = "img/cards-classic/" + this.value + this.suit + ".png";
+			var updateFn = function (){ 
+				card.view.setFill({image: card.view.image});
+				v52Instance.layer.draw();
+			};
+
+			if(this.view.image.complete){
+				updateFn();
+			}else{
+				this.view.image.onload = updateFn;
+			}
+		}else{
+			this.view.setFill("green");
+		}
+	}
+
+	return card;
+}
+
+
