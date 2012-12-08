18462543834dc0450bed60a0aa41cb3706cc03a6
Chris Sibbitt <chris@fractalcube.net>
2012-12-07 21:36:01 -0500

    I forget files alot and it usually results in duplicate comments. Sorry! It's because in rcs, cvs, and svn, a commit just commit everything, so I forget the -a

--- v52Server.js
+++ v52Server.js
@@ -1,37 +1,48 @@
-//Execute this like this:
-//node v52Server.js -p <port>
+//USAGE: start is like this
+// PORT=4444 node v52Server.js
 
-var express = require('express');
+var express = require('express')
+  , routes = require('./routes')
+  , http = require('http')
+  , path = require('path');
+
 var app = express();
 
-v52Server = function(){
-
-	//Parse command line options
-	var argv = require('optimist').argv;
-	this.port = argv.p ? argv.p : 5252;
-	console.info("Port is %d", this.port);
-
-	//Create a node-static file server
-	var static = require('node-static'); 
-	this.fileServer = new(static.Server)();
-
-	//Create an HTTP server and bind socket.io to it as well
-	var httpServer = require('http').createServer(app);
-	this.io = require('socket.io').listen(httpServer);
-
-	//Implement Chat - the easy version ;)
-	this.chatSocket = this.io.of('/chat').on('connection', function(socket){ 
-		socket.on('msg', function(m){
-			v52Server.chatSocket.emit('msg', m);
-		})
-	});
-
-	//Fire this sucker up
-	httpServer.listen(this.port);
-}
-
-app.get( /^\/|(.*\.html)|(.*\.js)$/ , function(req, res){
-	v52Server.fileServer.serve(req, res);
+app.configure(function(){
+  app.set('port', process.env.PORT || 5252); 
+  app.set('views', __dirname + '/views');
+  app.set('view engine', 'hjs');
+  app.use(express.favicon());
+  app.use(express.logger('dev'));
+  app.use(express.bodyParser());
+  app.use(express.methodOverride());
+  app.use(express.cookieParser('your secret here'));
+  app.use(express.session());
+  app.use(app.router);
+  app.use(express.static(path.join(__dirname, 'public')));
 });
 
-var v52Server = new v52Server();
+app.configure('development', function(){
+  app.use(express.errorHandler());
+});
+
+//Will set up routing later 
+//app.get('/', routes.index);
+
+//Create an HTTP server and bind socket.io to it as well
+var httpServer = http.createServer(app);
+app.v52io = require('socket.io').listen(httpServer);
+
+
+//Implement Chat - the easy version ;)
+app.v52chatSocket = app.v52io.of('/chat').on('connection', function(socket){ 
+	socket.on('msg', function(m){
+		app.v52chatSocket.emit('msg', m);
+	})
+});
+
+//Fire this sucker up
+httpServer.listen(app.get('port'), function(){
+	console.log("Express server listening on port " + app.get('port'));
+});
+
